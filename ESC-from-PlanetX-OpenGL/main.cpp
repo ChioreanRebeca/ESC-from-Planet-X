19ea20b23f604c0ead359eb5da18c16a60fbfbd8
Rebeca <163349981+ChioreanRebeca@users.noreply.github.com>
2024-11-16 13:30:43 +0200

    added needed configuration
    in order to support lab4 implementation
    i neded to add
    copy "$(ProjectDir)dependente\freeglut\freeglut.dll" "$(ProjectDir)$(ConfigurationName)"
    copy "$(ProjectDir)dependente\glew\glew32.dll" "$(ProjectDir)$(ConfigurationName)"
    in build events command line

--- ESC-from-PlanetX-OpenGL/main.cpp
+++ ESC-from-PlanetX-OpenGL/main.cpp
@@ -3,14 +3,38 @@
 #include <stdlib.h>
 #include <iostream>
 
+// Include GLEW
+#include "dependente\glew\glew.h"
+
 // Include GLFW
 #include "dependente\glfw\glfw3.h"
 
+// Include GLM
+#include "dependente\glm\glm.hpp"
+#include "dependente\glm\gtc\matrix_transform.hpp"
+#include "dependente\glm\gtc\type_ptr.hpp"
+
+#include "shader.hpp"
+
 //variables
 GLFWwindow* window;
-const int width = 1024, height = 768;
+const int width = 1024, height = 1024;
 
-int main()
+//Handling cursor position
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	std::cout << "The mouse cursor is: " << xpos << " " << ypos << std::endl;
+}
+
+//Ex 3: Add callback for mouse button
+
+//Ex 4: Complete callback for adjusting the viewport when resizing the window
+void window_callback(GLFWwindow* window, int new_width, int new_height)
+{
+	//what should we do here?
+}
+
+int main(void)
 {
 	// Initialise GLFW
 	if (!glfwInit())
@@ -20,7 +44,7 @@ int main()
 	}
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(width, height, "Our first test :)", NULL, NULL);
+	window = glfwCreateWindow(width, height, "Lab 4", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window.");
 		glfwTerminate();
@@ -29,48 +53,130 @@ int main()
 
 	glfwMakeContextCurrent(window);
 
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		glfwTerminate();
+		return -1;
+	}
+
 	//specify the size of the rendering window
 	glViewport(0, 0, width, height);
 
-	/*
-	For example, a processed point of location (-0.5,0.5)
-	would be mapped to (200,450)
-	in screen coordinates if our window is (800, 600).
-	Note that processed coordinates in OpenGL are between -1 and 1,
-	so we effectively map from the range (-1 to 1) to (0, 800) and (0, 600).
-	Where will (0.25, -0.5) be mapped?
-	*/
-
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-	float i = 0.0f;
-	float j = 0.0f;
+	glClear(GL_COLOR_BUFFER_BIT);
+
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+
+	GLfloat vertices[] = {
+		0.05f,  0.05f, 0.0f,  // top right
+		0.05f, -0.05f, 0.0f,  // bottom right
+		-0.05f, -0.05f, 0.0f,  // bottom left
+		-0.05f,  0.05f, 0.0f   // top left 
+	};
+
+	GLuint indices[] = {  // note that we start from 0!
+		0, 3, 1,  // first Triangle
+		1, 3, 2,   // second Triangle
+	};
+
+
+	// A Vertex Array Object (VAO) is an object which contains one or more Vertex Buffer Objects and is designed to store the information for a complete rendered object. 
+	GLuint vbo, vao, ibo;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ibo);
+
+	//bind VAO
+	glBindVertexArray(vao);
+
+	//bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//bind IBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//set attribute pointers
+	glVertexAttribPointer(
+		0,                  // attribute 0, must match the layout in the shader.
+		3,                  // size of each attribute
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		3 * sizeof(float),  // stride
+		(void*)0            // array buffer offset
+	);
+	glEnableVertexAttribArray(0);
+
+	//create matrices for transforms
+	glm::mat4 trans(1.0f);
+
+	//maybe we can play with different positions
+	glm::vec3 positions[] = {
+		glm::vec3(0.0f,  0.0f,  0),
+		glm::vec3(0.2f,  0.5f, 0),
+		glm::vec3(-0.15f, -0.22f, 0),
+		glm::vec3(-0.38f, -0.2f, 0),
+		glm::vec3(0.24f, -0.4f, 0),
+		glm::vec3(-0.17f,  0.3f, 0),
+		glm::vec3(0.23f, -0.2f, 0),
+		glm::vec3(0.15f,  0.2f, 0),
+		glm::vec3(0.15f,  0.7f, 0),
+		glm::vec3(-0.13f,  0.1f, 0)
+	};
+
+	// Set a callback for handling mouse cursor position
+	// Decomment for a callback example
+	// glfwSetCursorPosCallback(window, cursor_position_callback);
+
+	//Ex4 - Set callback for window resizing
+	glfwSetFramebufferSizeCallback(window, window_callback);
+
 
 	// Check if the window was closed
 	while (!glfwWindowShouldClose(window))
 	{
-		// Check for events
-		glfwPollEvents();
-
 		// Swap buffers
 		glfwSwapBuffers(window);
 
-		//if (i > 1.0f)
-		//{
-		//	i = 0.0f;
-		//}
-
-		//if (j > 1.0f)
-		//{
-		//	j = 0.0f;
-		//}
-
-		//glClearColor(i += 0.1f, 0.0f, j += 0.05f , 0.0f);
-
+		// Check for events
+		glfwPollEvents();
 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		//rotation task based on glfw
+		//trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+		//with fixed angle
+		trans = glm::rotate(trans, 1.0f, glm::vec3(0.0, 0.0, 1.0));
+
+		//bind VAO
+		glBindVertexArray(vao);
+
+		// send variables to shader
+		unsigned int transformLoc = glGetUniformLocation(programID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+		unsigned int transformLoc2 = glGetUniformLocation(programID, "color");
+		glm::vec4 color = glm::vec4(0.5f, 0, 0.5f, 1.0);
+		glUniform4fv(transformLoc2, 1, glm::value_ptr(color));
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	}
+
+	// Cleanup
+	glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &ibo);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteProgram(programID);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
